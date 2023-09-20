6b41ce58d08b740afd481763d153daecd3a5cc2a
rabehans <steeve.rabehanta@edu.esiee.fr>
2023-09-20 16:39:29 +0200

    Tilemap files

---
+++ levels/tilesets/terrain_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="terrain_tiles" tilewidth="32" tileheight="32" tilecount="160" columns="16">
+ <image source="../../images/terrain/pixel_platform_01_tileset_final.png" width="512" height="320"/>
+</tileset>
